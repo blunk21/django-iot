6afd25ee94d2f8c543c5528fce106550d29003dd
blunk21 <g.rockandroller@gmail.com>
2023-03-27 22:45:20 +0200

    trying dotenv

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,11 +84,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME':os.environ["MYSQLDATABASE"],
-        'USER': os.environ["MYSQLUSER"],
-        'HOST': os.environ["MYSQLHOST"],
-        'PASSWORD' : os.environ["MYSQLPASSWORD"],
-        'PORT':os.environ["MYSQLPORT"],
+        'NAME':os.environ.get("MYSQLDATABASE"),
+        'USER': os.environ.get("MYSQLUSER"),
+        'HOST': os.environ.get("MYSQLHOST"),
+        'PASSWORD' : os.environ.get("MYSQLPASSWORD"),
+        'PORT':os.environ.get("MYSQLPORT"),
 
     }
 }
